28490af2afdec3642846bfa2543c771223ac694c
Alaa <alaa.el-shamouty@outlook.com>
2023-07-24 17:14:13 +0200

    [CODE] Evaluation and tests

--- Evaluation/evaluation_playground.py
+++ Evaluation/evaluation_playground.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pickle
 
@@ -17,12 +18,12 @@ def plot_post_hoc_datatsets():
     x2 = df['nr_feature'].values
 
     fig.add_trace(
-        go.Scatter(x=x2, y=y, mode='markers', name='Nr features before FE',marker_color=colors[0]),
+        go.Scatter(x=x2, y=y, mode='markers', name='Nr features before FE', marker_color=colors[0]),
         row=1, col=1,
     )
 
     fig.add_trace(
-        go.Scatter(x=x1, y=y, mode='markers', name='Nr Features after FE ',marker_color=colors[1]),
+        go.Scatter(x=x1, y=y, mode='markers', name='Nr Features after FE ', marker_color=colors[1]),
         row=1, col=2
     )
     fig.add_trace(
@@ -30,7 +31,7 @@ def plot_post_hoc_datatsets():
                    hovertemplate=
                    '<br>Avg performance: %{y}<br>' +
                    '(before,after): %{text}',
-                   text=[f'({before},{after})' for after,before in zip(x1,x2) ], marker_color=colors[2] ),
+                   text=[f'({before},{after})' for after, before in zip(x1, x2)], marker_color=colors[2]),
         row=1, col=3
     )
 
@@ -175,6 +176,95 @@ def plot_performance_gain():
     fig.show()
 
 
+def get_opt_config_data():
+    directory = '../results_bo'
+    datasets = [458, 15, 1068, 11, 22, 16, 1510, 18, 31, 469, 188, 37, 54, 14, 23, 50]
+    rows = []
+    for dataset in datasets:
+        opt_config = os.path.join(directory, str(dataset), 'opt_cfg.json')
+        with open(opt_config) as json_file:
+            data = json.load(json_file)
+        data['id'] = dataset
+        rows.append(data)
+
+    df = pd.DataFrame(rows)
+    df.to_csv('opt_configurations_found.csv')
+
+
+def plot_post_hoc_bo():
+    data = pd.read_csv('opt_configurations_found.csv')
+    categorical_cols = ['children_per_step', 'max_pop_size',
+                        'population_size', 'selection_type',
+                        'total_number_of_function_evaluations']
+    fig = make_subplots(rows=1, cols=len(categorical_cols), shared_yaxes=True, horizontal_spacing=0.01)
+    for icol, col in enumerate(categorical_cols):
+        group = data.groupby([col])[col].count()
+        values = group.index.values
+        x = [str(val) for val in values]
+        y = [group[val] for val in values]
+        fig.add_trace(go.Bar(x=x, y=y, showlegend=False), row=1, col=icol + 1)
+        fig.update_xaxes(
+            title_text=col,
+            showline=True,
+            # showticklabels=True,
+            linecolor='black',
+            linewidth=2.4,
+            ticks='outside',
+            tickfont=font_dict,
+            mirror='allticks',
+            tickwidth=2.4,
+            tickcolor='black',
+            showgrid=True,
+            automargin=True
+
+        )
+    fig.update_layout(xaxis_type='category', bargap=0, bargroupgap=0)
+    # Change the bar mode
+    fig.update_yaxes(
+        title_text='Count',  # axis label
+        showline=True,  # add line at x=0
+        linecolor='black',  # line color
+        linewidth=2.4,  # line size
+        ticks='outside',  # ticks outside axis
+        tickfont=font_dict,  # tick label font
+        titlefont=font_dict,  # tick label font
+        mirror='allticks',  # add ticks to top/right axes
+        tickwidth=2.4,  # tick width
+        tickcolor='black',  # tick color
+        showgrid=True,
+        automargin=True,
+    )
+    fig.update_yaxes(title_standoff=0)
+
+    fig.update_layout(barmode='group',
+                      title='Optimal Configurations',
+                      template="simple_white",
+                      legend=dict(
+                          yanchor="top",
+                          y=0.9,
+                          xanchor="right",
+                          x=0.9,
+                          bordercolor="Black",
+                          borderwidth=1,
+                          bgcolor="rgba(255,255,255,0)",
+                          font=dict(family='Arial',
+                                    size=20,
+                                    color='black'
+                                    )
+                      ),
+                      font=font_dict,
+                      xaxis_title = 'Hyperparameters',
+                      )
+    fig.write_html('opt_configs.html')
+
+    #     if col == 'id':
+    #         continue
+    #     fig.add_trace(go.Box(y=data[col]))
+    # fig.write_image('opt_configs.html')
+
+
 if __name__ == '__main__':
+    #get_opt_config_data()
+    plot_post_hoc_bo()
     plot_post_hoc_datatsets()
     # plot_performance_gain()
--- tests/test_mutation.py
+++ tests/test_mutation.py
@@ -1,118 +1,22 @@
-from typing import Any, Optional, Tuple, Union
-
+import random
 import numpy as np
-from pytest import approx, mark, raises
+from collections import Counter
+from EA.strategies import Combiner
+from utilities import get_opr_name, get_inverse_weights
 
-from EA.evolution import Member, Mutation, Recombination
-from target_function import ackley
+def test_weighted_random_selection(seen_oprs=[]):
+    while True:
+        single_ops_filtered = [opr_info for opr_info in Combiner.single_ops if not (
+                get_opr_name(opr_info[0]) in seen_oprs and get_opr_name(opr_info[0]) in Combiner.one_time_oprs)]
 
-
-def make_member(
-    arr: Any,
-    method: Mutation,
-    bounds: Tuple[float, float] = (-30, 30),
-    sigma: Optional[float] = None,
-) -> Member:
-    return Member(
-        initial_x=np.asarray(arr),
-        model=ackley,
-        bounds=bounds,
-        mutation=method,
-        recombination=Recombination.NONE,
-        sigma=sigma,
-    )
-
-
-def test_none() -> None:
-    """
-    Expects
-    -------
-    * The coordinates and fitness should be equal between parent and child
-        when mutation does nothing
-    """
-    parent = make_member([0], Mutation.NONE)
-    child = parent.mutate()
-
-    assert parent.fitness == approx(child.fitness)
-    np.testing.assert_equal(parent.x_coordinate, child.x_coordinate)
-
-
-def test_uniform() -> None:
-    """
-    Expects
-    -------
-    * Mutating uniformly `n` times should produce `n` unique values, with some tolerance
-    """
-    member = make_member([0.0, 0.0], Mutation.UNIFORM)
-    n_mutations = 1_000
-    tolerance = 3
-
-    values = [member.mutate().x_coordinate for _ in range(n_mutations)]
-    unique = np.unique(values, axis=0)
-
-    assert len(unique) + tolerance >= len(values)
-
-
-@mark.parametrize("mutation, sigma", [(Mutation.NONE, None), (Mutation.UNIFORM, None), (Mutation.GAUSSIAN, 1.0)])
-def test_borders(mutation: Mutation, sigma: Union[None, float]) -> None:
-    """
-    Parameters
-    ----------
-    mutation: Mutation
-        The mutation method
-
-    sigma: Union[None, float]
-        The sigma to use, if relevant
-
-    Expects
-    -------
-    * A mutation should never go outside the bounds defined
-    """
-    n_mutations = 1_000
-
-    member = make_member([-1, -1], method=mutation, bounds=(-1, 1), sigma=sigma)
-    offspring = np.vstack([member.mutate().x_coordinate for _ in range(n_mutations)])
-
-    lower, upper = member._bounds
-    assert np.all((lower <= offspring) & (offspring <= upper))
-
-
-def test_gauss_raises_exception_with_no_sigma() -> None:
-    """
-    Expects
-    -------
-    * A member with a Guassian mutation strategy with no sigma set should raise an Error
-    """
-    member = make_member([0], Mutation.GAUSSIAN, sigma=None)
-    with raises(ValueError):
-        member.mutate()
-
-
-@mark.parametrize("x, sigma", [(np.array([0, 0]), 1.0), (np.array([10, -5]), 6.0)])
-def test_gauss(x: np.ndarray, sigma: float) -> None:
-    """
-    Parameters
-    ----------
-    x : np.ndarray
-        The starting coordinate of the Member
-
-    sigma : float
-        The sigma value to use
-
-    Expects
-    -------
-    * The children of a member with a guassian mutation should have a mean centered on
-    the original member and the correct stdev
-    """
-    n_mutations = 1_000
-    member = make_member(x, Mutation.GAUSSIAN, sigma=sigma)
-
-    offspring = np.vstack([member.mutate().x_coordinate for _ in range(n_mutations)])
-
-    mean = np.mean(offspring, axis=0)
-    stdev = np.std(offspring, axis=0)
-
-    expected_stdev = np.array([sigma, sigma])
-
-    np.testing.assert_allclose(x, mean, rtol=0.1, atol=0.3)
-    np.testing.assert_allclose(expected_stdev, stdev, rtol=0.1, atol=0.3)
+        mutation_seen_oprs = [seen_opr for seen_opr in seen_oprs if seen_opr in Combiner.single_ops_name]
+        if len(mutation_seen_oprs) > 0:
+            counter_seen_oprs = dict(Counter(mutation_seen_oprs))
+            weights_seen_oprs = get_inverse_weights(counter_seen_oprs, single_ops_filtered)
+            weights = weights_seen_oprs.values()
+            index = random.choices(np.arange(len(single_ops_filtered)), weights=weights)[0]
+            opr = get_opr_name(single_ops_filtered[index][0])
+            seen_oprs.append(opr)
+        else:
+            opr = random.choice(single_ops_filtered)[0]
+            seen_oprs.append(opr)
